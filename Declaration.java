70f0e67169f7bf3b0637a30a586d57f6bcfad1a6
Bhanumathi <bhanumathidotnet@gmail.com>
2016-07-10 16:37:43 +0530

    Assignment_Class1

---
+++ Declaration.java
@@ -0,0 +1,10 @@
+
+public class Declaration {
+
+	public static void main(String[] args) {
+		int a, b = 4, c;
+		System.out.println("The value of b is " + b);
+
+	}
+
+}
---
+++ TypeCast.java
@@ -0,0 +1,12 @@
+
+public class TypeCast {
+
+	public static void main(String[] args) {
+		 int a = 10;
+		 float b = 10.5f;
+		 int answer = (int) (a+b);
+		 System.out.println("The value is " + answer);
+
+	}
+
+}
